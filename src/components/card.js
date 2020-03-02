b83af5eb5147e801f5a27fe745d9e2f35f6ed145
shawnbmason <s.mason1993@yahoo.com>
2020-03-02 18:55:49 -0500

    Updated input

--- src/components/card.js
+++ src/components/card.js
@@ -3,13 +3,36 @@ import React, { Component } from 'react';
 import Input from './input';
 
 class Card extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      color: 'Blue',
+      pluralNoun: '',
+    };
+  }
+
+  handleInputChange() {
+    this.setState({ color: 'red' })
+  }
+
   render() {
+
+return(
+  <div className="card">
+    <h1>{this.state.color}</h1>
+    <input/>
+  </div>
+)
+
     return (
       <div className="card">
+        <h1>{this.state.color}</h1>
         { Input('Color') }
         { Input('Plural Noun') }
       </div>
-    )
+    );
   }
 }
 
