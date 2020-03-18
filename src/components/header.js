5f9879e51f0a24a89ad04cfdb4217d6bf8f8ecc3
shawnbmason <s.mason1993@yahoo.com>
2020-03-17 21:42:36 -0400

    style content component

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
       <div className="header">
         <div className="header__skew"></div>
-        <div className="header__border__bottom"></div>
+        <div className="header__border"></div>
         <h1>Bottega Madlibs</h1>
         <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
       </div>
