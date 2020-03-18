ed86412f8dae594985fff6c9228a86f0b3436a2f
shawnbmason <s.mason1993@yahoo.com>
2020-03-17 20:46:47 -0400

    styled generate madlib and clear from buttons

--- src/components/home.js
+++ src/components/home.js
@@ -7,11 +7,17 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-          { Header() }
-          <Card />
+        { Header() }
+        <Card />
       </div>
     );
   }
 }
+
+// things we need to fix
+// placeholder
+// gray and green number labels
+// content labels
+// generate btn space/card-height
 
 export default Home;
